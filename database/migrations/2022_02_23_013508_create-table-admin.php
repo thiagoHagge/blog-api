aae611a6361c6cf43a72b62b656adf0df4206c1e
Thiago Hagge <thiagohagge09@gmail.com>
2022-02-22 23:07:51 -0300

    delete default migrations

--- database/migrations/2022_02_23_013508_create-table-admin.php
+++ database/migrations/2022_02_23_013508_create-table-admin.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTableAdmin extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('auth_admin', function (Blueprint $table) {
+            $table->id('usr_id');
+            $table->string('usr_name');
+            $table->string('usr_pass');
+            $table->string('usr_token');
         });
+
     }
 
     /**
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::drop('auth_admin');
     }
 }
